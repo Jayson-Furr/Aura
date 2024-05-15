90f27484947ddd3c39e6c4af0bad9ad131f84198
Jayson Furr <jayson.furr@outlook.com>
2024-05-14 22:01:04 -0500

    Enhanced Input

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Jayson Furr
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
