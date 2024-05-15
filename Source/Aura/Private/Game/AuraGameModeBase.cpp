096c357af0243eb938c99e5f785d3577b70917aa
Jayson Furr <jayson.furr@outlook.com>
2024-05-14 23:05:13 -0500

    Game Mode

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Jayson Furr
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Jayson Furr
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
